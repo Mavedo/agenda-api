31c2cdcc54aae6e46331bae8edb5b0e774214b07
mavedo <m.vera.d@outlook.com>
2024-04-13 21:05:01 -0600

    Se está trabajando en la función de update, está incompleta

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ import re
 
 app = Flask(__name__)
 
+#Create database connection so it can be cosed after each query/action
 def open_mongo_db():
     """" It opens the connection with the mongo db.
     There after we can close it in every function, using client.close()"""
@@ -14,7 +15,8 @@ def open_mongo_db():
     except Exception as e:
         print(f'Error: {e}')
     return coll, client
-    
+
+#Read
 @app.route('/buscar', methods=['POST'])
 def look_up_contact():
     """ Function returns the result of looking up for a contact by name"""
@@ -36,6 +38,7 @@ def look_up_contact():
     client.close()
     return response
 
+#Create contact
 @app.route('/agregar', methods=['POST'])
 def new_contact():
     """We integrate all the information of the users input and add it to the MongoDB"""
@@ -85,8 +88,11 @@ def email_validation(email):
     email_regex = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$"
     return bool(re.match(email_regex, email))
 
+#Read
 @app.route('/nombres', methods=['GET'])
 def print_all_names(coll):
+    """ Está función se utiliza con el método GET, para retornar todos los nombres
+    de la base de datos. Solo los nombres."""
     coll, client = open_mongo_db()
     agenda = list(coll.find({},{'_id':0}))
     if len(agenda) == 0:
@@ -104,5 +110,39 @@ def print_all_names(coll):
     client.close()
     return response
 
+def update_contact():
+    coll, client = open_mongo_db()
+    name = request.args.get('nombre',type=str)
+    name_validation = name_in_agenda(name, coll)
+    value_to_update = request.args.get('dato',type=str).lower()
+    new_value = request.args.get('nuevo')
+    
+    if not name_validation:
+        response = jsonify({
+            "status_code":400,
+            "message":"Nombre del contacto no se encontró en la agenda"
+        })
+    if (value_to_update == 'numero' or 'telefono'):
+        if not number_validation(new_value):
+            response = jsonify({
+                "status_code":400,
+                "message": "El número de teléfono debe constar de 10 dígitos"
+            })
+        else:
+            coll.update_one({'nombre':name}, {'$set': {'numero': new_value}})
+            response = jsonify({
+                'status_code':200,
+                'message': f''
+            })
+        pass
+    elif (value_to_update == 'email' or 'mail'):
+        if not email_validation(new_value):
+            response = jsonify({
+                "status_code": 400,
+                "message":f"{new_value} no cumple con el formato de correo electrónico"
+            })
+
+def name_in_agenda(name, coll):
+    return coll.find_one({'nombre': {'$regex': f'^{re.escape(name)}$', '$options': 'i'}}) is not None
 
 app.run(debug=True, host='localhost', port=5000)
